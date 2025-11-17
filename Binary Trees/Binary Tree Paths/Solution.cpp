13278d3e152dbf553a6d26bf81ff3fa5ad5bb28c
satyanshu111 <satyanshu624@gmail.com>
2025-11-17 19:29:22 +0530

    Binary Tree Paths

---
+++ Binary Trees/Binary Tree Paths/Solution.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+vector<string>paths;
+void dfs(TreeNode* u, string &cur){
+     if(!u){
+        return;
+     }
+     int old_sz = cur.size();
+
+     if(!cur.empty())cur+= "->";
+     cur += to_string(u->val);
+
+     if(!u->left && !u->right){
+        paths.push_back(cur);
+     }
+     else{
+        dfs(u->left, cur);
+        dfs(u->right, cur);
+     }
+
+     cur.resize(old_sz);
+     
+}
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+          string cur="";
+          paths.clear();
+
+          dfs(root, cur);
+          return paths;
+    }
+};
---
+++ Binary Trees/Binary Tree Paths/readme.md
@@ -0,0 +1,9 @@
+# Binary Tree Paths
+
+**Category:** Binary Trees  
+**Platform:** LeetCode  
+**Link:** https://leetcode.com/problems/binary-tree-paths/
+
+## Complexity
+- **Time:** O(N^2)  
+- **Space:** O(N^2)
